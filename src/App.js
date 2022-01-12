0924473183ecb95bb508df4aca8e2891a557847b
xsalvatore <xavier.salvatore.w@gmail.com>
2022-01-12 17:27:18 -0500

    Added all the files and updated the README

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import Data from './data/content.json';
+
+const App = () => {
+  const [activeFilter, setActiveFilter] = useState('all');
+  const [filters, setFilters] = useState([]);
+
+  const movies = Data.movies;
+
+  useEffect(() => {
+    let genres = [];
+
+    for (let i = 0; i < movies.length; i ++) {
+      genres.push(movies[i].genre);
+    }
+
+    setFilters([...new Set(genres.flat())]);
+  }, [setActiveFilter]);
+
+  const onChange = e => {
+    setActiveFilter(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="filtering">
+        <form>
+          <select name="genre" id="genre" onChange={onChange}>
+            {filters.map((filter, index) => {
+              return (
+                <option key={index} value={filter}>{filter}</option>
+              );
+            })}
+          </select>
+        </form>
+      </div>
+
+      <div className="gallery">
+        {movies.map((movie, index) => {
+          return (
+            <div key={index} className={`card ${activeFilter === 'all' ? 'show-all' : ''} ${movie.genre.includes(activeFilter) ? 'show' : 'hide'}`}>
+              <img src={`./images/${movie.image_name}`} alt="" />
+              
+              <div className="info">
+                <h2>{movie.title}</h2>
+                
+                <div className="badges">
+                  {movie.genre.map((genre, index) => {
+                  return (
+                    <div key={index}>
+                      <p>{genre}</p>
+                    </div>
+                  );})}
+                </div>
+                  
+                <p className="summary">{movie.plot}</p>
+
+                <a href={movie.link}>Visit Movie</a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
